c74d0a849d8a10689b640a970585d746a355d825
EliasCorazon <102911055+EliasCorazon@users.noreply.github.com>
2022-07-22 03:33:53 +0500

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,15 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> allUsers();
+    User findById(Long id);
+    User addUser(User addUser);
+    void delete(Long id);
+    void edit(Long id, User editUser, String[] role, String password);
+    User getUsername(String name);
+}
